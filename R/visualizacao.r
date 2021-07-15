b87197b34e23162deca3cc5602d573da81df6bcd
Lucas De Souza Khenayfis <lucask@ons.org.br>
2021-07-15 18:06:04 -0300

    Recurso experimental de translacao do mapa

--- R/visualizacao.r
+++ R/visualizacao.r
@@ -4,9 +4,16 @@
 #' 
 #' Funcao facilitadora para visualizacao do dado de temperatura na superficie 
 #' 
+#' O parametro \code{wrap} permite plotar diferentes centralizacoes do mapa mundi. Supondo que 
+#' observamos o globo de cima, a primeira posivao do vetor \code{wrap} indica o grau inicial do mapa
+#' e, a segunda, o final. O padrao de \code{wrap = c(0, 360)} corresponde ao plot centrado no oceano
+#' pacifico, enquanto \code{wrap = c(-180, 180)} corresponde a centralizacao no atlantico. Veja os
+#' exemplos.
+#' 
 #' @param dado o dado do qual extrair serie. Deve ter formato igual ao retornado por 
 #'     \code{\link{leFROMdir}} ou \code{\link{leFROMurl}}
 #' @param data escalar string no formato AAAAMM indicando o ano-mes que deve ser plotado
+#' @param warp \emph{EXPERIMENTAL} vetor indicando os limites do mapa. Ver Detalhes
 #' @param outdir opcional, indicando diretorio para escrita dos graficos. Ver Exemplos
 #' 
 #' @examples 
@@ -16,6 +23,9 @@
 #' 
 #' # janela 202001:202003
 #' plots <- plotamapa(datexemplo, "202001:202003")
+#' 
+#' # plot centralizado no atlantico
+#' plotamapa(datexemplo, "202001", wrap = c(-180, 180))
 #' 
 #' \dontrun{
 #' # escrita do arquivo em um diretorio qualquer
@@ -30,7 +40,10 @@
 #' 
 #' @export
 
-plotamapa <- function(dado, data, outdir) {
+plotamapa <- function(dado, data, wrap = c(0, 360), outdir) {
+
+    # distorcao pelo wrap fornecido
+    if(!(abs(diff(wrap)) == 360)) stop("Wrap nao representa um arco de 360 graus")
 
     if(missing("data")) {
         data <- dado$DATE[1]
@@ -39,22 +52,32 @@ plotamapa <- function(dado, data, outdir) {
     }
 
     dplot <- dado[DATE %in% data]
+    mapa  <- map_data("world", wrap = wrap)
+
+    lon0 <- dplot$LON
+    dplot[lon0 < min(wrap), LON := LON + 360]
+    dplot[lon0 > max(wrap), LON := LON - 360]
+    setorder(dplot, LON)
 
     # O raster assume que cada par LON, LAT e um centro do retangulo a ser desenhado. Isso cria uma
     # ilusao de offset entre os mapas e raster, porque o primeiro quadrado em LON = 0 fica esticado
     # pra uma longitude negativa
     # Para corrigir isso temos que repetir o trecho de LON = 0 para LON = 360, pois o quadrante em
     # LON = 0 inclui trechos do outro lado do mapa (maps o plot nao faz essa cicliidade)
-    aux <- dplot[LON == 0]
-    aux[, LON := max(dplot$LON) + 2]
-    dplot <- rbind(dplot, aux)
+    if((min(dplot$LON) - 1) < min(mapa$long)) {
+        aux <- dplot[LON == min(LON)]
+        aux[, LON := max(dplot$LON) + 2]
+        dplot <- rbind(dplot, aux)
+    } else if((max(dplot$LON) + 1) > max(mapa$long)) {
+        aux <- dplot[LON == max(LON)]
+        aux[, LON := min(dplot$LON) - 2]
+        dplot <- rbind(dplot, aux)
+    }
 
     # Algo similar acontece pros limites de lat
     aux <- dplot[LAT == min(LAT)]
     aux <- aux[, LAT := LAT - 2]
     dplot <- rbind(dplot, aux)
-
-    mapa <- map_data("world", wrap = c(0, 360))
 
     limsst <- dplot[, range(SST, na.rm = TRUE)]
     midsst <- dplot[, mean(SST, na.rm = TRUE)]
